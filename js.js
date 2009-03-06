da749aed85d60c567ad7807cf66981bde09860b9
dsimard <dsimard@gmail.com>
2009-03-06 17:48:09 -0500

    sending files

---
+++ js.js
@@ -0,0 +1,60 @@
+$("document").ready(function() {
+	$("h1, h2").hide();
+
+	var lapse = function() {
+		setTimeout(lightning, 800+(Math.random()*3200));
+	}
+
+	var lightning = function() {
+		nbImages = 3;
+		$("#lightning")
+			.css("backgroundImage", "url(back"+Math.round(1+(Math.random()*(nbImages))).toString()+".jpg)")
+			.css("backgroundPosition", Math.round(-300+(Math.random()*300)).toString()+"px 0px")
+			.show()
+			.fadeOut(850+(Math.random()*300), lapse);
+			//document.title = $("#lightning").css("backgroundImage");
+	}
+	lapse();
+
+  var phrases = ["It all began with a great idea",
+    /*"That came to life in a sudden.",
+    "It was Unfinished but promising",
+    "and we stumbled and failed",
+    "We let the idea grow",
+    "And what was once an ambitious experiment",
+    "Has evolved into a complete solution",
+    "Where all pieces now fit together.",
+    "It's no more a time tracking tool over IM",
+    "It's the result of our initial vision",
+    "and what it was meant to be.",
+    "We understand",
+    "that with great potential",
+    "comes great expectations",*/
+   ]
+
+	var showPhrase = function() {
+		if (phrases.length > 0) {
+			setTimeout(function() {
+				$("<span>")
+					.hide()
+					.text(phrases[0])
+					.css("top", Math.round(Math.random()*300)+"px")
+					.css("left", Math.round(Math.random()*300)+"px")
+					.fadeIn(3500, function() {
+						$(this).fadeTo(3500, 0.001, function() { showPhrase(); })
+					}).insertBefore($("#lightning"));
+			
+				phrases.shift();
+
+			}, 1000)
+		} else {
+			$("h1").hide().fadeIn(8000, function() {
+				$("h2").fadeIn(6000, function() {
+					$("h3").fadeIn(5000);
+				});
+			});
+		}
+	};
+	
+	showPhrase();
+});
